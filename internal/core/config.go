d8af51948cc7906e17adba6ba53062dd3207ea82
popcorny <celu@infuseai.io>
2022-02-15 00:51:43 +0800

    Implment config command
    Signed-off-by: popcorny <celu@infuseai.io>

--- internal/core/config.go
+++ internal/core/config.go
@@ -1,32 +1,23 @@
 package core
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
+	"strings"
 
 	"github.com/BurntSushi/toml"
 )
-
-type ArtConfig struct {
-	Repo string    `toml:"repo"`
-	S3   *S3Config `toml:"s3"`
-}
-
-type S3Config struct {
-	AwsAccessKeyId *string `toml:"aws_access_key_id"`
-	AwsAccessKey   *string `toml:"aws_secret_access_key"`
-	Region         *string `toml:"region"`
-}
 
 func s(str string) *string {
 	return &str
 }
 
 func InitRepo(baseDir string) error {
-	config := ArtConfig{}
+	config := make(map[string]interface{})
 
 	airDir := path.Join(baseDir, ".art")
 	configPath := path.Join(airDir, "config")
@@ -51,20 +42,25 @@ func InitRepo(baseDir string) error {
 	return nil
 }
 
-func LoadConfig() (ArtConfig, error) {
+type ArtConfig struct {
+	config map[string]interface{}
+	path   string
+}
 
-	load := func(dir string) (ArtConfig, error) {
-		var config ArtConfig
+func LoadConfig() (*ArtConfig, error) {
+
+	load := func(dir string) (map[string]interface{}, error) {
+		var config = make(map[string]interface{})
 		configPath := path.Join(dir, ".art/config")
 
 		data, err := ioutil.ReadFile(configPath)
 		if err != nil {
-			return config, err
+			return nil, err
 		}
 
 		err = toml.Unmarshal(data, &config)
 		if err != nil {
-			return config, err
+			return nil, err
 		}
 
 		return config, nil
@@ -72,14 +68,20 @@ func LoadConfig() (ArtConfig, error) {
 
 	dir, err := os.Getwd()
 	if err != nil {
-		return ArtConfig{}, err
+		return nil, err
 	}
 
 	for {
 		config, err := load(dir)
-		if err == nil {
-			return config, nil
+		if errors.As(err, &toml.ParseError{}) {
+			fmt.Fprintf(os.Stderr, "cannot load the workspace config\n")
+			return nil, err
 		}
+
+		if err == nil {
+			return &ArtConfig{config: config, path: path.Join(dir, ".art/config")}, nil
+		}
+
 		newDir := filepath.Dir(dir)
 		if dir == newDir {
 			break
@@ -87,5 +89,80 @@ func LoadConfig() (ArtConfig, error) {
 		dir = newDir
 	}
 
-	return ArtConfig{}, fmt.Errorf("cannot find art repository")
+	return nil, fmt.Errorf("cannot find art repository")
+}
+
+func (config *ArtConfig) Set(path string, value interface{}) {
+	var obj map[string]interface{} = config.config
+
+	parts := strings.Split(path, ".")
+	for i, p := range parts {
+		if i == len(parts)-1 {
+			obj[p] = value
+		} else {
+			if v, ok := obj[p].(map[string]interface{}); ok {
+				obj = v
+			} else {
+				child := make(map[string]interface{})
+				obj[p] = child
+				obj = child
+			}
+		}
+	}
+}
+
+func (config *ArtConfig) Get(path string) interface{} {
+	var obj interface{} = config.config
+	var val interface{} = nil
+
+	parts := strings.Split(path, ".")
+	for _, p := range parts {
+		if v, ok := obj.(map[string]interface{}); ok {
+			obj = v[p]
+			val = obj
+		} else {
+			return nil
+		}
+	}
+
+	return val
+}
+
+func (config *ArtConfig) Print() {
+	var printChild func(string, interface{})
+
+	printChild = func(path string, obj interface{}) {
+		if v, ok := obj.(map[string]interface{}); ok {
+			for key, value := range v {
+				if path == "" {
+					printChild(key, value)
+				} else {
+					printChild(path+"."+key, value)
+				}
+			}
+		} else {
+			fmt.Printf("%s=%v\n", path, obj)
+		}
+	}
+
+	printChild("", config.config)
+}
+
+func (config *ArtConfig) Save() error {
+	f, err := os.OpenFile(config.path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+
+	if err := toml.NewEncoder(f).Encode(config.config); err != nil {
+		return err
+	}
+
+	err = f.Close()
+	if err != nil {
+		return err
+
+	}
+
+	return nil
 }
