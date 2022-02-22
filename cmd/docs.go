823d8da06d510823f800864f3bea15d4fb47e1e7
Ching Yi, Chan <chingyichan.tw@gmail.com>
2022-02-22 16:02:50 +0800

    Fix false-error (#5)
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

--- cmd/docs.go
+++ cmd/docs.go
@@ -17,7 +17,8 @@ art docs`,
 	Run: func(cmd *cobra.Command, args []string) {
 		const DocDir = "./generated_docs"
 		err := os.Mkdir(DocDir, fs.ModePerm)
-		if err != nil && os.IsExist(err) {
+
+		if err == nil || (err != nil && os.IsExist(err)) {
 			// pass when directory existing
 		} else {
 			fmt.Printf("Failed to create %s, skip to generate documents\n", DocDir)
